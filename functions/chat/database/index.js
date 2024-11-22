d006bde1f9e218719dae69ab9eb7f34692863cf0
Franklin <franklinbarbs763@gmail.com>
2024-11-21 22:41:30 -0300

    refactor(chat-database): should change sort of get all by coordinates

--- functions/chat/database/index.js
+++ functions/chat/database/index.js
@@ -38,7 +38,7 @@ class ChatDataBase {
                                 $centerSphere: [[params.lat, params.long], radiusInRadians],
                             },
                         },
-                    }).sort({ _id: -1 })).map(chat => chat.toObject());
+                    }).sort({ _id: 1 })).map(chat => chat.toObject());
                     return chats;
                 }
                 catch (error) {
